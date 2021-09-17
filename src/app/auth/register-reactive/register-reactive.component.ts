828524a2f262a8fcc02e70fbaf36421e6c036fdb
mitin <v.mitin@mobidev.biz>
2021-09-17 11:44:37 +0300

    BAD CODE - pushing temporary changes, breaks build

--- src/app/auth/register-reactive/register-reactive.component.ts
+++ src/app/auth/register-reactive/register-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConfirmPasswordValidator, } from 'src/app/shared/validators/confirm-password.validator';
 
 @Component({
   selector: 'app-register-reactive',
@@ -7,16 +8,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register-reactive.component.css']
 })
 export class RegisterReactiveComponent implements OnInit {
-  profileForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required)
-  });
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl('', Validators.required),
+  //   lastName: new FormControl('', Validators.required),
+  //   email: new FormControl('', Validators.required),
+  //   password: new FormControl('', Validators.required),
+  //   confirmPassword: new FormControl('', Validators.required)
+  // },
 
-  constructor() { }
-
-  ngOnInit(): void {
-    // this.profileForm.favoriteColorControl.setValue('Set value');
+  constructor(public fb: FormBuilder) {
   }
+
+  registerForm: FormGroup;
+
+
+
+  ngOnInit() {
+    this.registerForm = this.fb.group(
+      {
+        email: ["", Validators.required],
+        password: ["", Validators.required],
+        confirmPassword: ["", Validators.required]
+      },
+      {
+        validator: ConfirmPasswordValidator('password', 'confirmPassword')
+      })
+  };
 
   submit() {
     console.log(this.profileForm.value);
