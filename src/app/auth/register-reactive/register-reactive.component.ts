1cee4b45ae65a9f520854ef944a787f7f8492833
Victor <lolwner@gmail.com>
2021-09-19 20:11:19 +0300

    reactive components

--- src/app/auth/register-reactive/register-reactive.component.ts
+++ src/app/auth/register-reactive/register-reactive.component.ts
@@ -8,31 +8,24 @@ import { ConfirmPasswordValidator, } from 'src/app/shared/validators/confirm-pas
   styleUrls: ['./register-reactive.component.css']
 })
 export class RegisterReactiveComponent implements OnInit {
-  // profileForm = new FormGroup({
-  //   firstName: new FormControl('', Validators.required),
-  //   lastName: new FormControl('', Validators.required),
-  //   email: new FormControl('', Validators.required),
-  //   password: new FormControl('', Validators.required),
-  //   confirmPassword: new FormControl('', Validators.required)
-  // },
 
   constructor(public fb: FormBuilder) {
   }
 
-  registerForm: FormGroup;
-
+  profileForm = this.fb.group(
+    {
+      firstName: [""],
+      lastName: [""],
+      email: [""],
+      password: [""],
+      confirmPassword: [""]
+    },
+    {
+      validator: ConfirmPasswordValidator('password', 'confirmPassword')
+    })
 
 
   ngOnInit() {
-    this.registerForm = this.fb.group(
-      {
-        email: ["", Validators.required],
-        password: ["", Validators.required],
-        confirmPassword: ["", Validators.required]
-      },
-      {
-        validator: ConfirmPasswordValidator('password', 'confirmPassword')
-      })
   };
 
   submit() {
